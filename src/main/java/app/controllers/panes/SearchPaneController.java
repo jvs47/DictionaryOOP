ffa725660723a14514f379a235fd6ece3e02ef7d
jvs47 <77630688+jvs47@users.noreply.github.com>
2021-10-19 18:10:26 +0700

    prepare SearchPaneController

--- src/main/java/app/controllers/panes/SearchPaneController.java
+++ src/main/java/app/controllers/panes/SearchPaneController.java
@@ -1,5 +1,14 @@
 package app.controllers.panes;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SearchPaneController extends ViewController{
 
+    public Button searchButton;
+
+    @FXML
+    public void handleSearchButtonEvent(ActionEvent actionEvent) {
+    }
 }
