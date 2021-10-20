cd43a363a64e51f5b782b55812d6c146849e8bb8
jvs47 <77630688+jvs47@users.noreply.github.com>
2021-10-20 13:37:20 +0700

    modify size

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("container.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 750, 480);
+        Scene scene = new Scene(fxmlLoader.load(), 750, 485);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
