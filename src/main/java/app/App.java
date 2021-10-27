b9862d1fb640ebca044bc6115b9699be1de382fe
jvs47 <77630688+jvs47@users.noreply.github.com>
2021-10-27 15:27:51 +0700

    smooth Font script

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.setProperty("prism.lcdtext", "false"); // smooth font
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("container.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 485);
         stage.setTitle("Hello!");
