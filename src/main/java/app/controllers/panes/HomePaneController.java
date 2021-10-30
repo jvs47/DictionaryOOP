ce8f475fb6f30da6557ef8b839bba403d6c08837
jvs47 <77630688+jvs47@users.noreply.github.com>
2021-10-30 08:19:22 +0700

    set action Home buttons

--- src/main/java/app/controllers/panes/HomePaneController.java
+++ src/main/java/app/controllers/panes/HomePaneController.java
@@ -1,4 +1,35 @@
 package app.controllers.panes;
 
-public class HomePaneController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class HomePaneController{
+    @FXML
+    private Label aboutHomeButton;
+
+    @FXML
+    private Label favouriteHomeButton;
+
+    @FXML
+    private Label onlineSearchHomeButton;
+
+    @FXML
+    private Label searchHomeButton;
+    private ContainerController stage;
+
+    @FXML
+    void handleHomeButtonClick(MouseEvent event) {
+        if (event.getSource() == searchHomeButton) {
+            stage.showSearchPane();
+        } else if (event.getSource() == favouriteHomeButton) {
+            stage.showFavouritePane();
+        } else if (event.getSource() == onlineSearchHomeButton) {
+            stage.showOnlineEVPane();
+        }
+    }
+
+    public void initData(ContainerController stage) {
+        this.stage = stage;
+    }
 }
