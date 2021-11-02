a698aa8a6643f4ddecfecdd6de2fe5f420e4ff36
jvs47 <johnvu23.vn@gmail.com>
2021-11-02 17:46:50 +0700

    implement and deploy Pronounce Function

--- src/main/java/app/controllers/panes/OnlineGoogleSearchController.java
+++ src/main/java/app/controllers/panes/OnlineGoogleSearchController.java
@@ -1,15 +1,18 @@
 package app.controllers.panes;
 
+import app.helper.AudioGoogleAPI;
 import app.online.GoogleTransAPI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javazoom.jl.decoder.JavaLayerException;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-public class OnlineGoogleSearchController extends ViewController {
+public class OnlineGoogleSearchController {
     private ContainerController stage;
     private boolean modeEV;
     @FXML
@@ -23,6 +26,9 @@ public class OnlineGoogleSearchController extends ViewController {
     @FXML
     private TextArea meaningOnlineEVTextArea, meaningOnlineVETextArea;
     @FXML
+    private Label listenEN, listenVN;
+
+    @FXML
     void onMouseClickSwitchEVLanguage(MouseEvent event) {
         stage.showOnlineVEPane();
         modeEV = false;
@@ -34,10 +40,37 @@ public class OnlineGoogleSearchController extends ViewController {
         modeEV = true;
     }
 
+    @FXML
+    void handleListenEVClick(MouseEvent event) throws IOException, JavaLayerException {
+        if (event.getSource() == listenEN) {
+            AudioGoogleAPI audio = AudioGoogleAPI.getInstance();
+            InputStream sound = audio.getAudio(inputOnlineEVTextArea.getText(), "en");
+            audio.play(sound);
+        } else if (event.getSource() == listenVN) {
+            AudioGoogleAPI audio = AudioGoogleAPI.getInstance();
+            InputStream sound = audio.getAudio(meaningOnlineEVTextArea.getText(), "vi");
+            audio.play(sound);
+        }
+    }
+
+    @FXML
+    void handleListenVEClick(MouseEvent event) throws IOException, JavaLayerException {
+        if (event.getSource() == listenEN) {
+            AudioGoogleAPI audio = AudioGoogleAPI.getInstance();
+            InputStream sound = audio.getAudio(inputOnlineVETextArea.getText(), "vi");
+            audio.play(sound);
+        } else if (event.getSource() == listenVN) {
+            AudioGoogleAPI audio = AudioGoogleAPI.getInstance();
+            InputStream sound = audio.getAudio(meaningOnlineVETextArea.getText(), "en");
+            audio.play(sound);
+        }
+    }
+
     public void initData(ContainerController stage, boolean modeEV) {
         this.stage = stage;
         this.modeEV = modeEV;
     }
+
     @FXML
     void handleOnlineEVInput(KeyEvent event) throws IOException, org.json.simple.parser.ParseException {
         if (event.getSource() == inputOnlineEVTextArea) {
