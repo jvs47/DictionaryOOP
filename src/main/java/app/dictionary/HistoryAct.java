c178c8f7ef3b400c9665739d8783e15e7faaf201
NguyenDucAnh <ducanhkhtn@gmail.com>
2021-11-03 16:35:33 +0700

    no change

--- src/main/java/app/dictionary/HistoryAct.java
+++ src/main/java/app/dictionary/HistoryAct.java
@@ -14,51 +14,51 @@ public class HistoryAct extends DictionaryAct {
 
     }
 
-    public void importFromDatabase() {
-        TreeMap<String, String> history = new TreeMap<>();
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT word, description FROM avHistory");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next() == true) {
-                history.put(resultSet.getString(1), resultSet.getString(2));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Done insert history from database!");
-    }
-
-    public void addToHistoryDatabase(String foundWord) {;
-        String sql = "INSERT INTO avHistory(word, html, description, pronounce) SELECT word, html, description, pronounce FROM av WHERE word = ?";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, foundWord);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public TreeMap<String, String> addToHistoryString(String foundWord) {
-        TreeMap<String, String> history = new TreeMap<>();
-        String sql = "SELECT word, description FROM av";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next() == true) {
-                history.put(resultSet.getString(1), resultSet.getString(2));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return history;
-    }
-
-    public ArrayList<String> toArrayString() {
-        TreeMap<String, String> words = this.getTreeWord();
-        ArrayList<String> wordArrays = new ArrayList<>();
-        for (Map.Entry<String, String> entry : words.entrySet()) {
-            wordArrays.add(entry.getKey());
-        }
-        return wordArrays;
-    }
+//    public void importFromDatabase() {
+//        TreeMap<String, String> history = new TreeMap<>();
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement("SELECT word, description FROM avHistory");
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            while (resultSet.next() == true) {
+//                history.put(resultSet.getString(1), resultSet.getString(2));
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("Done insert history from database!");
+//    }
+//
+//    public void addToHistoryDatabase(String foundWord) {;
+//        String sql = "INSERT INTO avHistory(word, html, description, pronounce) SELECT word, html, description, pronounce FROM av WHERE word = ?";
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//            preparedStatement.setString(1, foundWord);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public TreeMap<String, String> addToHistoryString(String foundWord) {
+//        TreeMap<String, String> history = new TreeMap<>();
+//        String sql = "SELECT word, description FROM av";
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            while (resultSet.next() == true) {
+//                history.put(resultSet.getString(1), resultSet.getString(2));
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return history;
+//    }
+//
+//    public ArrayList<String> toArrayString() {
+//        TreeMap<String, String> words = this.getTreeWord();
+//        ArrayList<String> wordArrays = new ArrayList<>();
+//        for (Map.Entry<String, String> entry : words.entrySet()) {
+//            wordArrays.add(entry.getKey());
+//        }
+//        return wordArrays;
+//    }
 }
