8f37825f4a02c31e20e7c089e54cce17385b0e3c
jvs47 <johnvu23.vn@gmail.com>
2021-11-04 18:57:20 +0700

    add function to Search Button

--- src/main/java/app/controllers/panes/SearchPaneController.java
+++ src/main/java/app/controllers/panes/SearchPaneController.java
@@ -1,17 +1,3 @@
 package app.controllers.panes;
-
-import app.dictionary.Word;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-
 public class SearchPaneController extends ViewController {
-
-    public Button searchButton;
-
-    @FXML
-    public void handleSearchButtonEvent(ActionEvent actionEvent) {
-        System.out.println("Click Search!");
-    }
 }
